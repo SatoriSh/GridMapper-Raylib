9bcfa29ea52401beb1a69b6d83d8f3393560e1ad
SatoriSh <satorizoom51@gmail.com>
2026-02-01 17:55:27 +0100

    added cells render func

--- grid/grid.cpp
+++ grid/grid.cpp
@@ -1,9 +1,15 @@
+#include <cmath>
 #include "grid.h"
 
-Grid::Grid() : backgroundColor(GRAY)
+Grid::Grid() : gridWidth(30), gridHeight(30), cellSize(16)
 {
-    InitWindow(screenWidth, screenHeight, "GridMapper-Raylib");
-    SetTargetFPS(FPS);
+    initWindow();
+
+    camera = {0};
+    camera.target = {0.0f, 0.0f};
+    camera.offset = {(float)GetScreenWidth() / 2.0f, (float)GetScreenHeight() / 2.0f}; 
+    camera.zoom = 1.0f;                                
+    camera.rotation = 0.0f;
 }
 
 void Grid::process()
@@ -13,13 +19,35 @@ void Grid::process()
         BeginDrawing();
         ClearBackground(backgroundColor);
 
+        Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+        int mouseGridX = (int)floor(mouseWorldPos.x / cellSize);
+        int mouseGridY = (int)floor(mouseWorldPos.y / cellSize);
+
+        BeginMode2D(camera);
+        render();
+        EndMode2D();
+
+        DrawText(TextFormat("MouseX: %d\n\nMouseY: %d", mouseGridX, mouseGridY), 15, 15, 20, WHITE);
         EndDrawing();
     }
 }
 
 void Grid::render()
 {
+    for (int y = 0; y < gridHeight; y++)
+    {
+        for (int x = 0; x < gridWidth; x++)
+        {
+            DrawRectangleLines(x * cellSize, y * cellSize, cellSize, cellSize, cellLinesColor);
+            DrawText("0", x * cellSize + cellSize / 4, y * cellSize + cellSize / 4, 10, GREEN);
+        }
+    }
+}
 
+void Grid::initWindow()
+{
+    InitWindow(screenWidth, screenHeight, "GridMapper-Raylib");
+    SetTargetFPS(FPS);
 }
 
 Grid::~Grid()
--- grid/grid.h
+++ grid/grid.h
@@ -13,6 +13,14 @@ class Grid
     const int screenWidth = 1280;
     const int screenHeight = 720;
     const int FPS = 60;
-    const Color backgroundColor;
+    const Color backgroundColor = {30, 80, 140, 255};
+    const Color cellLinesColor = {100, 150, 200, 255};
+    int gridWidth;
+    int gridHeight;
+    int cellSize;
     void render();
+    void initWindow();
+
+
+    Camera2D camera;
 };
